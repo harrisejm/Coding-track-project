b7599b03bd5ba48a0b6b5bb2065c7f1f48318325
Eddie Harris <harrisejm@gmail.com>
2018-06-01 11:38:03 -0700

    add additional questions to questionaire

--- js/scripts.js
+++ js/scripts.js
@@ -15,21 +15,24 @@ $(document).ready(function() {
       $("div.panel-body p.p4").slideToggle();
   });
 
-
+  $(".toggle5").click(function(){
+      $("div.panel-body p.p5").slideToggle();
+  });
 
   $("#formOne").submit(function(event) {
     var name = $("#name").val();
   var question1 = $("input:radio[name=question1]:checked").val();
   var question2 = $("input:radio[name=question2]:checked").val();
+  var question3 = $("input:radio[name=question3]:checked").val();
+  var question4 = $("input:radio[name=question4]:checked").val();
   //  var age = parseInt(ageString);
     //var job = $("#job").val();
 
 //var beverage = $("#beverage").val();
-
-
-
     $(".info1").text(question1);
     $(".info2").text(question2);
+    $(".info3").text(question3);
+    $(".info4").text(question4);
   //  $(".info2").text("Job: " + job);
 
 
