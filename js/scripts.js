0025c98776cbda73e9d04b1bec0b6888611c2a7b
Eddie Harris <harrisejm@gmail.com>
2018-06-01 09:32:31 -0700

    add side up/down feature to language description on home page

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".toggle1").click(function(){
+      $("div.panel-body p.p1").slideToggle();
+  });
+
+  $(".toggle2").click(function(){
+      $("div.panel-body p.p2").slideToggle();
+  });
+
+  $(".toggle3").click(function(){
+      $("div.panel-body p.p3").slideToggle();
+  });
+
+  $(".toggle4").click(function(){
+      $("div.panel-body p.p4").slideToggle();
+  });
+
+});
