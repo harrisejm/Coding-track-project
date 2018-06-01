b0c219e1690ba4f68808c1c98c4a323e83dd1d25
Eddie Harris <harrisejm@gmail.com>
2018-06-01 11:05:06 -0700

    add radio forms to questionaire page

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,33 @@ $(document).ready(function() {
       $("div.panel-body p.p4").slideToggle();
   });
 
+
+
+  $("#formOne").submit(function(event) {
+    var name = $("#name").val();
+  var question1 = $("input:radio[name=question1]:checked").val();
+  var question2 = $("input:radio[name=question2]:checked").val();
+  //  var age = parseInt(ageString);
+    //var job = $("#job").val();
+
+//var beverage = $("#beverage").val();
+
+
+
+    $(".info1").text(question1);
+    $(".info2").text(question2);
+  //  $(".info2").text("Job: " + job);
+
+
+    //$("#story").show();
+
+    event.preventDefault();
+  });
+
+
+
+
+
+
+
 });
