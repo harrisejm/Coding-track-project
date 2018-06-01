ca756232713ed5c122b25d346f1d0666e574b840
Eddie Harris <harrisejm@gmail.com>
2018-06-01 13:00:03 -0700

    add conditional statements for #C option

--- js/scripts.js
+++ js/scripts.js
@@ -20,8 +20,8 @@ $(document).ready(function() {
   });
 
   $("#formOne").submit(function(event) {
-    var name = $("#name").val();
-  var question1 = $("input:radio[name=question1]:checked").val();
+  var name = $("#name").val();
+  var question1 = $("input:radio[name=question1]:checked").val(); debugger;
   var question2 = $("input:radio[name=question2]:checked").val();
   var question3 = $("input:radio[name=question3]:checked").val();
   var question4 = $("input:radio[name=question4]:checked").val();
@@ -29,14 +29,22 @@ $(document).ready(function() {
   //  var age = parseInt(ageString);
     //var job = $("#job").val();
 
-//var beverage = $("#beverage").val();
+
     $(".info1").text(question1);
     $(".info2").text(question2);
     $(".info3").text(question3);
     $(".info4").text(question4);
     $(".info5").text(question5);
-  //  $(".info2").text("Job: " + job);
 
+if (question1 === "no" && question2 === "software" && question3 === "yes") {
+   $(".results").text("Although you indicated you do not like MicroSoft C#");
+} else if (question1 === "no" && question2 === "software" && question3 === "no") {
+   $(".results").text("C# for sure");
+} else if (question1 === "yes" && question2 === "software" && question3 === "yes") {
+  $(".results").text("Although you indicated you do not like MicroSoft C#");
+} else if (question1 === "yes" && question2 === "software" && question3 === "no") {
+   $(".results").text("C# for sure");
+}
 
     //$("#story").show();
 
